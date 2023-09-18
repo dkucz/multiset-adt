4bf2ca3dc52383c4376a55ca9842c947776b037a
Dominik Kucz <dominik.kucz@mail.utoronto.ca>
2023-09-18 12:29:56 -0400

    Abstract multiset class added

---
+++ src/Multiset.java
@@ -0,0 +1,15 @@
+abstract class Multiset {
+    public static void main(String[] args) {
+
+    }
+    public Multiset(){
+
+    }
+
+    public abstract boolean add();
+    public abstract boolean contains();
+    public abstract boolean is_empty();
+    public abstract int count();
+    public abstract int size();
+    public abstract void remove();
+}
